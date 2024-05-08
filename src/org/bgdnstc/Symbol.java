aaa11ff8209fa0cdf46d912beb6849f45d3fd175
unknown <bogdanstoica10@gmail.com>
2024-05-08 20:26:34 +0300

    format symbol class

--- src/org/bgdnstc/Symbol.java
+++ src/org/bgdnstc/Symbol.java
@@ -1,11 +1,18 @@
 package org.bgdnstc;
 
 public enum Symbol {
+    // variable types
     TYPE_INT, TYPE_FLOAT, TYPE_STRING, SOCKET, UDP_SERVER, UDP_CLIENT,
+    // values
     INT, FLOAT, STRING,
+    // operators
     EQUALS, ADD, SUB, MUL, DIV, LOOP, WHILE, PRINT,
+    // methods
     RECEIVE, SEND,
+    // punctuation marks
     DOT, L_BRACKET, R_BRACKET, L_PARENTHESIS, R_PARENTHESIS, AT,
+    // identifier (variable name)
     IDENTIFIER,
+    // markers (end of line)
     EOL
 }
