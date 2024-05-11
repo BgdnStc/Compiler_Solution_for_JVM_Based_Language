f609fe9e295273bf73a34d10e9026c9f789b29e2
unknown <bogdanstoica10@gmail.com>
2024-05-11 16:24:49 +0300

    File format checked in source reader.

--- src/org/bgdnstc/SourceReader.java
+++ src/org/bgdnstc/SourceReader.java
@@ -4,17 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class SourceReader {
+    private static final Pattern byteNetFile = Pattern.compile(".*\\.bynt$");
 
     private SourceReader() {
     }
 
     protected static String readSource(Path path) {
         try {
-            return Files.readString(path);
+            if (byteNetFile.matcher(path.toString()).matches()) {
+                return Files.readString(path);
+            } else {
+                throw new InvalidPathException(path.toString().substring(path.toString().lastIndexOf("\\")) , "Invalid file format provided. ByteNet code is stored exclusively in \".bynt\" files. File provided");
+            }
         } catch (IOException e) {
-            throw new InvalidPathException("Invalid provided path:" + path + ". ", "Path should be from the current content root or an absolute path.");
+            throw new InvalidPathException(path.toString(), "Invalid path provided. The path should have the root the current content directory or absolute path. Path provided");
         }
     }
 }
