69aa1d78c82f36fb292c7a187d1c188fd659c053
unknown <bogdanstoica10@gmail.com>
2024-07-21 02:11:07 +0300

    final version

--- src/org/bgdnstc/Main.java
+++ src/org/bgdnstc/Main.java
@@ -2,8 +2,8 @@ package org.bgdnstc;
 
 public class Main {
     public static void main(String[] args) {
-        // JDK 21
-        // asm-9.6
+        // Java version: JDK 21
+        // Object ASM library version: asm-9.6
 
         for (String path : args) {
             System.out.println(path);
